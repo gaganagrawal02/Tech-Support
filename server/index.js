75c27e9d44111fc430dcb9fd42c233ae4b62ddc9
Gagan Agrawal <134318471+gaganagrawal02@users.noreply.github.com>
2024-12-19 12:51:35 +0530

    index change

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 const serviceSlots = [
   {
